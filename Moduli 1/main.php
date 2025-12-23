dd648fcc9e3db4b230321c006bd04539a76192cf
sufjanz <sufjanzogaj@gmail.com>
2025-12-23 20:22:39 +0100

    moduli 1

---
+++ Moduli 1/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 1</title>
+    <style>
+        h1 {
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <h1>Max Vertsappen</h1>
+    <?php
+    echo "PHP/MySQL ";
+    ?>
+</body>
+</html>
